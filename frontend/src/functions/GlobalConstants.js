95019f76114ad126c31a2cca894e5d86facf3231
martinhug <mhug@hsr.ch>
2019-12-12 17:54:06 +0100

    Global Constants + package.json

--- frontend/src/functions/GlobalConstants.js
+++ frontend/src/functions/GlobalConstants.js
@@ -2,18 +2,6 @@
  * Palette of global color presets.
  */
 export const palette = {
-    yellow: '#FFFF08',
-    black: '#000000',
-    white: '#FFFFFF',
-    lightpurple: '#D1C4E9',
-    purple: '#B39DDB',  // #B388FF
-    blue: '#90CAF9',
-    darkblue: '#29B6F6',
-    green: '#81C784',
-    jade: '#4DB6AC',
-    red: '#F06292',
-    ruby: '#EC407A',
-    orange: '#FF7043',
     topologydesignerblue: '#2d6480',
 };
 
